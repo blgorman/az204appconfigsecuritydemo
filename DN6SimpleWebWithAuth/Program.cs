f017c842405afe710c561589886974eb38bbc4db
Brian Gorman <blgorman@gmail.com>
2022-07-04 22:42:43 -0500

    Update Program.cs
    force migrations

--- DN6SimpleWebWithAuth/Program.cs
+++ DN6SimpleWebWithAuth/Program.cs
@@ -11,6 +11,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+//automatically apply database migrations (breaks solution if database not wired up correctly, forces roll-forward approach
+var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlServer(connectionString).Options;
+using (var context = new ApplicationDbContext(contextOptions))
+{
+    context.Database.Migrate();
+}
+
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
